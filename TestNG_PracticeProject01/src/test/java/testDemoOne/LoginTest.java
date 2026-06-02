382e5985092a8748f315ace0760479e1ed92e6b6
rokde <rokde@Acer.lan>
2026-06-02 15:57:17 +0530

    Fixed cart functionality test using explicit wait and improved locators

--- TestNG_PracticeProject01/src/test/java/testDemoOne/LoginTest.java
+++ TestNG_PracticeProject01/src/test/java/testDemoOne/LoginTest.java
@@ -4,60 +4,74 @@ import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class LoginTest {
-WebDriver driver;
 
-@BeforeClass
-public void steUp() {
-	driver = new ChromeDriver();
-	driver.navigate().to("https://www.saucedemo.com/");
-	driver.manage().window().maximize();
-	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
-	driver.manage().deleteAllCookies();
-}
-@Test
-public void logintest() throws InterruptedException {
-	driver.findElement(By.xpath("//input[@id='user-name']")).sendKeys("standard_user");
-	driver.findElement(By.xpath("//input[@id='password']")).sendKeys("secret_sauce");
-	driver.findElement(By.xpath("//input[@id='login-button']")).click();
-	Thread.sleep(2000);
+	WebDriver driver;
+	WebDriverWait wait;
 
-}
+	@BeforeClass
+	public void steUp() {
 
-@Test(dependsOnMethods = {"logintest"})
-public void verifyLogin() {
-	String actual_title= driver.getTitle();
-	String expected_title= "Swag Labs";
-	Assert.assertEquals(actual_title , expected_title);
-	
-}
+		driver = new ChromeDriver();
+		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
-@Test(dependsOnMethods = {"logintest"})
-public void verifyCartFunctinality() throws InterruptedException {
-	driver.findElement(By.xpath("//button[@id='add-to-cart-sauce-labs-backpack']")).click();
-	Thread.sleep(2000);
-	driver.findElement(By.xpath("//span[@class='shopping_cart_badge']")).click();
-	Thread.sleep(2000);
-	String actual_text=driver.findElement(By.xpath("//span[@class='title']")).getText();
-	String expected_text= "Your Cart";
-	Assert.assertEquals(actual_text, expected_text);
-	
-}
+		driver.navigate().to("https://www.saucedemo.com/");
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+	}
 
-@AfterClass
-public void tearDown() {
-	driver.navigate().back();
-	driver.navigate().refresh();
-	driver.quit();
-	
-}
+	@Test(priority = 1)
+	public void logintest() {
+
+		driver.findElement(By.id("user-name")).sendKeys("standard_user");
+		driver.findElement(By.id("password")).sendKeys("secret_sauce");
+		driver.findElement(By.id("login-button")).click();
+
+		wait.until(ExpectedConditions.visibilityOfElementLocated(
+				By.id("add-to-cart-sauce-labs-backpack")));
+	}
+
+	@Test(priority = 2)
+	public void verifyLogin() {
+
+		String actual_title = driver.getTitle();
+		String expected_title = "Swag Labs";
+
+		Assert.assertEquals(actual_title, expected_title);
+	}
+
+	@Test(priority = 3)
+	public void verifyCartFunctinality() {
+
+		driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
+
+		WebElement cartBadge = wait.until(
+				ExpectedConditions.visibilityOfElementLocated(
+						By.className("shopping_cart_badge")));
+
+		cartBadge.click();
+
+		String actual_text = wait.until(
+				ExpectedConditions.visibilityOfElementLocated(
+						By.className("title"))).getText();
+
+		String expected_text = "Your Cart";
+
+		Assert.assertEquals(actual_text, expected_text);
+	}
+
+	@AfterClass
+	public void tearDown() {
+
+		driver.quit();
+	}
 }
